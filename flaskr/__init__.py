ccd0d980f0cb97994e096cba09991137b9c12623
leonardo ojeda <nahuelojeda2007@gmail.com>
2025-06-02 11:31:53 -0300

    termine el ejercicio 5 'blog blueprint'

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -7,8 +7,10 @@ def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite')
     )
+   
+ 
 
     if test_config is None:
          app.config.from_pyfile('config.py', silent=True)
@@ -31,4 +33,9 @@ def create_app(test_config=None):
     from . import db
     db.init_app(app)
 
+    from . import blog
+    app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint='index')
+
     return app
+  
